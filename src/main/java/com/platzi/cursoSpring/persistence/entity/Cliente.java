ecf4fe0863aa1c1a40e6ed596bf6cf2bb81d8ae9
luvera <lvera@exisoft.com.ar>
2023-07-03 15:00:37 -0300

    Agregado de entities

---
+++ src/main/java/com/platzi/cursoSpring/persistence/entity/Cliente.java
@@ -0,0 +1,65 @@
+package com.platzi.cursoSpring.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    private String id;
+    private String nombre;
+    @Column(name = "apellidos")
+    private String apellido;
+    private Long celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
